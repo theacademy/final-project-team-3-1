a32b8a53a66fa745f89210c6fbbba35a20e9f3eb
Emerson Havener <emerson@nevada.unr.edu>
2024-01-26 09:05:47 -0800

    Throws an exception at startup to ensure that the directory for uploaded images is found to avoid a silent error that can break image loading.

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+# Art Marketplace
+
+## Setup
+
+Ensure that `app.uploads.location` in `application.properties` is set to the location of `backend/uploads`
--- backend/src/main/java/com/team3/shop/MvcConfig.java
+++ backend/src/main/java/com/team3/shop/MvcConfig.java
@@ -1,15 +1,30 @@
 package com.team3.shop;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    @Value("${app.uploads.location}")
+    private String uploadsLocation;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        if (!Files.exists(Paths.get(uploadsLocation))) {
+            throw new StorageLocationNotFoundException("\n\n\n ERROR: Uploads location not found: " + uploadsLocation + ". " +
+                    "Please set the app.uploads.location value in application.properties to point to your 'backend/uploads' directory. \n" +
+                    "Alternatively, you can comment out this check in McvConfig.java. " +
+                            "Images will not load and you will not be able to edit or add products via the dashboard."
+                    + "\n\n\n");
+        }
+
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
+                .addResourceLocations("file:" + uploadsLocation);
     }
 }
