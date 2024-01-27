f41ce21762c236ec1037941486465eb4db32dc98
jalay <jalayeri.hoda@gmail.com>
2024-01-27 11:54:18 -0600

    fix error

--- backend/src/main/java/com/team3/shop/MvcConfig.java
+++ backend/src/main/java/com/team3/shop/MvcConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Configuration
@@ -16,7 +18,15 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        if (!Files.exists(Paths.get(uploadsLocation))) {
+
+        // Get the current working directory (root of the project)
+        Path currentPath = Paths.get(System.getProperty("user.dir"));
+
+        // Construct the full path by appending the folder names
+        Path fullPath = currentPath.resolve("backend").resolve(uploadsLocation);
+
+
+        if (!Files.exists(fullPath)) {
             throw new StorageLocationNotFoundException("\n\n\n ERROR: Uploads location not found: " + uploadsLocation + ". " +
                     "Please set the app.uploads.location value in application.properties to point to your 'backend/uploads' directory. \n" +
                     "Alternatively, you can comment out this check in McvConfig.java. " +
@@ -25,6 +35,6 @@ public class MvcConfig implements WebMvcConfigurer {
         }
 
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:" + uploadsLocation);
+                .addResourceLocations("file:\\" + fullPath+ "/");
     }
 }
